6bdf512542c36587f4021ad26be54b9aab93558f
dakshyab <47439324+dakshyab@users.noreply.github.com>
2021-05-27 14:54:27 +0545

    Initial commit

---
+++ spam_phishing_site.py
@@ -0,0 +1,49 @@
+import requests
+import os
+import random
+import string
+
+#Character list for password creation and a random seed to initialize the number generation
+chars = string.ascii_letters + string.digits + '!@#%^&()'
+random.seed = (os.urandom(1024))
+
+#random email provider generator
+emails= ['@gmail.com','@yahoo.com','@hotmail.com','@outlook.com']
+
+#url containing phsing site
+url = 'http://tengbdata.xyz/'
+
+
+def randomString(stringLength=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+#opening text file containing a list of names
+with open("lnames.txt") as f:
+    numedefamilie = f.readlines()
+
+numedefamilie = [x.strip() for x in numedefamilie]
+
+with open("lnames.txt") as f:
+    prenume = f.readlines()
+
+prenume = [x.strip() for x in prenume]
+
+while True:
+    numef_n = random.choice(numedefamilie)
+    prenume_n = random.choice(prenume)
+
+    username = numef_n + prenume_n
+    username = username.lower() + random.choice(emails)
+
+
+    password = ''.join(random.choice(chars) for i in range(8))
+    
+
+    r=requests.post(url, allow_redirects = False, data = {'email': username,'pass': password})
+    
+    #printing the status of every loop and the request
+    print ('sending username: %s and password: %s' % (username, password))
+    print("POST Request made.")
+    print(r.status_code, r.reason)
+    print("\n")
